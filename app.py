52614887bcbade3161f445c9e1d03b89cd29f44d
Lloyd Draizen Martirez <llmartirez@my.cspc.edu.ph>
2025-03-04 21:25:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, send_file
-from pytubefix import YouTube, exceptions
-import os
+from pytubefix import YouTube
 from urllib.error import HTTPError
 
 app = Flask(__name__)
@@ -13,28 +12,30 @@ def index():
 def download():
     link = request.form['youtube_link']
     try:
-        yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
+        yt = YouTube(link)
         audio_stream = yt.streams.filter(only_audio=True).first()
         video_stream = yt.streams.get_highest_resolution()
     except HTTPError as e:
         return f"An error occurred while accessing the video: {e}"
-    except exceptions.BotDetection:
-        return "Bot detection triggered. Please try again later or use a different link."
-    except Exception as e:
-        return f"An error occurred: {e}"
 
     choice = request.form['download_choice']
-    try:
-        filename = f"{yt.title}.{choice}"
-        if choice == "mp4":
+    if choice == "mp4":
+        try:
+            filename = f"{yt.title}.mp4"
             video_stream.download(filename=filename)
-        elif choice == "mp3":
+            return send_file(filename, as_attachment=True)
+        except Exception as e:
+            return f"An error occurred while downloading the video: {e}"
+    elif choice == "mp3":
+        try:
+            filename = f"{yt.title}.mp3"
             audio_stream.download(filename=filename)
-        else:
-            return "Invalid choice. Please enter 'mp4' or 'mp3'."
-        return send_file(filename, as_attachment=True)
-    except Exception as e:
-        return f"An error occurred while downloading the {choice}: {e}"
+            return send_file(filename, as_attachment=True)
+        except Exception as e:
+            return f"An error occurred while downloading the audio: {e}"
+    else:
+        return "Invalid choice. Please enter 'mp4' or 'mp3'."
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
